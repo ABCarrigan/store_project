02db80b27dbac8176561268802c98fe646096222
ABCarrigan <addisonbcarrigan@gmail.com>
2022-03-18 16:23:16 -0400

    edit page working

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -1,5 +1,4 @@
 const React = require('react');
-// As you can see we are using the app layout
 const DefaultLayout = require('./layout/Default.jsx')
 
 class Edit extends React.Component{
@@ -8,14 +7,14 @@ class Edit extends React.Component{
       <DefaultLayout title="Edit Page">
         <link rel="stylesheet" href="/css/app.css"/>      
         {/* The Layout takes in a prop called Title and we pass the Edit Page to it */}
-        <form action={`/products/${this.props.products._id}?_method=PUT`} method="POST">
-        Name: <input type="text" name="name" defaultValue={this.props.products.name}/><br/>
-        Image Link: <input type="text" name="img"  defaultValue={this.props.products.img}/><br/>
-        Description: <input type="text" name="description" defaultValue={this.props.products.description}/>
+        <form action={`/products/${this.props.product._id}?_method=PUT`} method="POST">
+        Name: <input type="text" name="name" defaultValue={this.props.product.name}/><br/>
+        Image Link: <input type="text" name="img"  defaultValue={this.props.product.img}/><br/>
+        Description: <input type="text" name="description" defaultValue={this.props.product.description}/>
         <br></br>
-        Price: <input type="number" name="price" defaultValue={this.props.products.price}/>
+        Price: <input type="number" min="0" step="0.01" name="price" defaultValue={this.props.product.price}/>
         <br></br>
-        Quantity: <input type="number" name="quantity" defaultValue={this.props.products.qty}/>
+        Quantity: <input type="number" min="0" name="qty" defaultValue={this.props.product.qty}/>
         <br/>
         <br/>
         <input type="submit" value="Submit Changes"/>
