dac0d0d7f88c033d0d4df24c7ef56c23cb6a29c8
ABCarrigan <addisonbcarrigan@gmail.com>
2022-03-18 12:56:17 -0400

    updated index view to pull from mongoDB

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-// allows us to connect to mongoose db
+// connecting to mongo db
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,12 +1,11 @@
 const React = require('react');
-const Product = require('../models/products');
 
 class Index extends React.Component {
   render() {
     return (<div>
         <link rel="stylesheet" href="/css/app.css"/>
         <h1>Products Index Page</h1>
-        { Product.map((product,i)=>{
+        { this.props.products.map((product,i)=>{
             return (<div key = {i}>
                 <img href={`/products/${product.img}`}></img>
                 </div>)
